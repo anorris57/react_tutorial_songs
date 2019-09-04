fe6af83535f98c85f46c5d0fa03e8d6fd7b56ef4
Angela Norris <cocoanorris@yahoo.com>
2019-09-04 16:21:21 -0400

    created  action creator and named export

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,2 +1,11 @@
 //pattern used in react projects of using index.js
 //short cut webpack looks for index.js file if no file specified
+//Action Creator
+export const selectSong = (song) => {
+  //Return an action
+  return {
+    type: 'SONG_SELECTED',
+    payload: song
+  };
+};
+
