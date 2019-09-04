ef27ef3052eb48c03e31f452ad5e6c6226bb9e71
Angela Norris <cocoanorris@yahoo.com>
2019-09-04 16:39:40 -0400

    creating  reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+  return [
+    { title: 'You Say', duration: '4:40'},
+    { title: 'So Will I', duration: '7:30'},
+    { title: 'Fear No More', duration: '3:14'},
+    { title: 'Let It Rain', duration: '3:29'}
+  ]
+};
+
+//if statement used just in case app is expanded
+const selectedSongReducer = (selectedSong = null, action) => {
+  if(action.type ==='SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+}
